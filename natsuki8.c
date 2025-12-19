a5a17e81651611a62092d7e313083fb930c19227
102422anm-rgb <102422anm@gmail.com>
2025-12-19 19:26:44 +0900

    Add files via upload

--- natsuki8.c
+++ natsuki8.c
@@ -1,50 +1,28 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    char password[100];
-    int i = 0;
-    int len;
-    int hasLower = 0;
-    int hasUpper = 0;
-
-    printf("パスワードを入力してください >>> ");
-    fgets(password, sizeof(password), stdin);
-
-    /* 改行を除いた文字数を取得 */
-    len = strlen(password);
-    if (password[len - 1] == '\n') {
-        password[len - 1] = '\0';
-        len--;
+    srand(time(0));
+    
+    int intelligence = (rand() % 18) + 1;
+    int stamina = (rand() % (19-intelligence)) + 1;
+    int charisma=(20-intelligence-stamina);
+    
+    printf("%d\n",intelligence);
+    printf("%d\n",stamina);
+    printf("%d\n",charisma);
+    
+    if(intelligence>=stamina&&intelligence>=charisma)
+{
+    printf("mage \n");
+}
+    else if(stamina>=intelligence&&stamina>=charisma){
+    printf("knight \n");
     }
-
-    /* while文で1文字ずつ判定 */
-    while (i < len) {
-        if (password[i] >= 'a' && password[i] <= 'z') {
-            hasLower = 1;
-        }
-        if (password[i] >= 'A' && password[i] <= 'Z') {
-            hasUpper = 1;
-        }
-        i++;
+    else{
+    printf("thief \n");
     }
-
-    /* 改善点の表示 */
-    if (len < 8) {
-        printf("8文字以上に変更してください。\n");
-    }
-    if (!hasLower) {
-        printf("小文字が含まれていません。\n");
-    }
-    if (!hasUpper) {
-        printf("大文字が含まれていません。\n");
-    }
-
-    /* 強いパスワード判定 */
-    if (len >= 8 && hasLower && hasUpper) {
-        printf("パスワードは強力です。\n");
-    }
-
     return 0;
 }
